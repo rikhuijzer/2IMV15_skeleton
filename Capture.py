b4e7c7d019a926ee2f525483dff7fcb79cd96281
rikhuijzer <rikhuijzer@users.noreply.github.com>
2018-05-20 15:55:49 +0200

    refactoring

--- Capture.py
+++ Capture.py
@@ -3,17 +3,12 @@ import cv2
 
 
 class Capture:
-    w: int
-    h: int
-    fourcc: cv2.VideoWriter_fourcc
     out: cv2.VideoWriter()
 
     def __init__(self, w, h):
-        self.w = w
-        self.h = h
-        self.fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+        four_cc = cv2.VideoWriter_fourcc(*'MJPG')
         fps = 60.0
-        self.out = cv2.VideoWriter('output.avi', self.fourcc, fps, (self.w, self.h))
+        self.out = cv2.VideoWriter('output.avi', four_cc, fps, (w, h))
 
     def record(self, data: np.ndarray):
         self.out.write(data)
